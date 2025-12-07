944a4e8a2c4c4b4f88e5ae2f0c57ac1d8491b4f4
sumanth raju <sumanthraju4449@gmail.com>
2025-12-07 22:04:18 +0530

    completed

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,1 +1,1 @@
-export const api_base_url = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+export const api_base_url = import.meta.env.VITE_API_BASE_URL || "https://blog-app-1cwg.onrender.com";
